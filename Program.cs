4181228d85a56b1b1381f9fe6e532f4cb0138866
Hitesh Chhillar <hitesh.chhillar16@gmail.com>
2021-07-17 18:37:33 -0400

    Initial load of the full project

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Mail;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace SMTPUtility
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //file in disk
+            var FileUrl = @"C:\DEV\projects\SMTPUtility\data\Addresses.txt";
+            string[] lines = File.ReadAllLines(FileUrl);
+            var addressList = new List<string>();
+            foreach (string line in lines)
+            {
+                var lineList = new List<string>();
+                lineList = line.Trim().Split(',').Where(x => x != "").ToList();
+                addressList.AddRange(lineList);
+            }
+
+            foreach (var address in addressList)
+            {
+                // Command-line argument must be the SMTP host.
+                try{
+                SmtpClient smtpClient = new SmtpClient("mxdevutl01.mxdev.int");
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("hitesh.test@matrixcare.com",
+                                            "Hitesh " + (char)0xD8 + " Chhillar", System.Text.Encoding.UTF8),
+                    Subject = "subject",
+                    Body = "<h1>Hello</h1>",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add(address);
+                mailMessage.Body = "This is a test email message sent by an application using address from notepad. ";
+                smtpClient.Send(mailMessage);
+                smtpClient.Dispose();
+                }catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+    }
+}
